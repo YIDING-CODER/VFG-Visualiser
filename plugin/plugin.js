31c4908096bcf50ed3436550a9e5cc83d678efda
YIDING-CODER <yid3@student.unimelb.edu.au>
2021-07-17 16:32:08 +1000

    Replaced the File Chooser
    Add Animation Profile Chooser, but the code needs to be rectified.

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -5,27 +5,7 @@ var root, d3, zoom, viewerWidth, viewerHeight;
 // Heuristic globals
 var hSim, svgID, heursvg, svgCount=1, actions, fluents, fluentPreconditions = {}, formattedActions, heurdata;
 
-// Called when you click 'Go' on the file chooser
-function loadStatespace() {
 
-    // Getting string versions of the selected files
-    var domain = window.ace.edit($('#domainSelection').find(':selected').val()).getSession().getValue();
-    var problem = window.ace.edit($('#problemSelection').find(':selected').val()).getSession().getValue();
-
-    window.heuristicVizDomain = domain;
-    window.heuristicVizProblem = problem;
-
-    // Lowering the choose file modal menu
-    $('#chooseFilesModal').modal('toggle');
-    $('#plannerURLInput').show();
-
-    // Ground the domain and problem
-    ground(domain, problem).then(function(result) {
-        treeData = {"name":"root", "children":[], "state":result.state, "strState":result.strState, "precondition":null, "loadedChildren":false};
-        stateCounter = 1;
-        launchViz();
-    });
-}
 
 function launchViz(){
     window.new_tab('Viz2.0', function(editor_name){
@@ -41,6 +21,7 @@ function launchViz(){
       '    </div>' +
       '    <div id="statename" style="clear:both">State</div>' +
       '    <div id="statedetails" style="padding:10px"></div>' +
+      '    <div id="planimation" style="padding:10px"></div>'+
       '  </div>' +
       '</div>' +
       '<node circle style ="fill:black;stroke:black;stroke-width:3px;></node circle>' +
@@ -1046,10 +1027,184 @@ function autoUpdate(graph, hAdd, hUpdate=true) {
     return graph.get('goal').value;
 }
 
+/*
+--------------------------------------------------------------------------------
+                                START OF Viz and Planimation File Choose
+--------------------------------------------------------------------------------
+*/
+var PLANIMATION_MODEL = `
+<!-- Choose Files Modal -->
+<div class="modal fade" id="chooseFilesPlanimationModel" tabindex="-1" role="dialog" aria-labelledby="chooseFilesModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="display:inline" id="chooseFilesModalLabel">Planimate your plan</h4> | 
+        <a onclick="$('#planimationhelpModal').modal(true)">  more info  <span class="glyphicon glyphicon-question-sign" style="cursor: pointer; top:7px !important; left:7px;font-size:25px;"aria-hidden="true"></span></a>
+    
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal left" role="form">
+          <div class="form-group">
+            <label for="domainPlanimationSelection" class="col-sm-4 control-label">Domain</label>
+            <div class="col-sm-8">
+              <select id="domainPlanimationSelection" class="form-control file-selection">
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="problemPlanimationSelection" class="col-sm-4 control-label">Problem</label>
+            <div class="col-sm-8">
+              <select id="problemPlanimationSelection" class="form-control file-selection">
+              </select>
+            </div>
+          </div>
+           <div class="form-group">
+            <label for="animateSelection" class="col-sm-4 control-label">Animation</label>
+            <div class="col-sm-8">
+              <select id="animateSelection" class="form-control file-selection">
+              </select>
+            </div>
+          </div>
+       
+        </form>
+
+        <button id="filesChosenButton" class="btn-lg btn-success" type="button" onclick="filesChosen()">Planimate</button>
+    
+
+
+        <div class="form-group" style="display:inline-block">
+            
+
+         
+
+        <div id="plannerURLInput" class="input-group">
+          <input type="radio" id="urlradio" name="planradio"  onchange="on_change(this)" checked style="display:flex;position:relative;top:-10px;margin-left:15px;margin-right:-10px;">
+          <span class="input-group-addon" id="customPlannerLabel">Custom Planner URL</span>
+          <input id="plannerPlanimationURL" type="text" class="form-control" aria-describedby="customPlannerLabel" placeholder="http://solver.planning.domains/solve">
+        </div>
+
+<br/>
+            <div class="col-sm-4" style="margin-bottom:5px;">
+            <input type="radio" id="planradio" name="planradio" onchange="on_change(this)" style="margin-right:10px">
+            <label>Upload Plan</label>
+            </div>
+
+            <div class="col-sm-4" style="position:relative;top:-5px;left:-6px;">
+              <select id="planSelection" style="display:none" class="form-control file-selection">
+              </select>
+            </div>
+          
+      </div>
+      <br/>
+
+      <div class="modal-footer"  >
+        <a href="http://planimation.planning.domains/" style="float:left" target="_blank">Try Planimation Web App</a>
+        <button type="button" class="btn btn-default"  data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
+`
+
+
+function choosePlanimationFiles(type) {
+
+    window.action_type = type
+    window.file_choosers[type].showChoice();
+  
+    var domain_option_list = "";
+    var problem_option_list = "";
+    var animate_option_list = "";
+    var plan_option_list = "";
+    var unknown_option_list = "";
+    var hr_line = "<option disabled=\"disabled\">---------</option>\n";
+    var setDom = false;
+    var setProb = false;
+    var setAnimate = false;
+    var setPlan = false;
+  
+    for (var i = 0; i < window.pddl_files.length; i++) {
+      if ($.inArray(window.pddl_files[i], window.closed_editors) == -1) {
+        if (window.pddl_files[i] == window.last_domain)
+          setDom = true;
+        if (window.pddl_files[i] == window.last_problem)
+          setProb = true;
+        if (window.pddl_files[i] == window.last_animate)
+          setAnimate = true;
+        if (window.pddl_files[i] == window.last_plan)
+          setPlan = true;
+  
+        var option = "<option value=\"" + window.pddl_files[i] + "\">" + $('#tab-' + window.pddl_files[i]).text() + "</option>\n";
+        var file_text = window.ace.edit(window.pddl_files[i]).getSession().getValue();
+        if (file_text.indexOf('(domain') !== -1)
+          domain_option_list += option;
+        else if (file_text.indexOf('(problem') !== -1)
+          problem_option_list += option;
+        else if (file_text.indexOf('(animation') !== -1)
+          animate_option_list += option;
+        else
+          unknown_option_list += option;
+      }
+    }
+  
+    var domain_list = domain_option_list + hr_line + unknown_option_list + hr_line + problem_option_list;
+    var problem_list = problem_option_list + hr_line + unknown_option_list + hr_line + domain_option_list;
+    var animate_list = animate_option_list + hr_line + unknown_option_list + hr_line + animate_option_list;
+    var plan_list = plan_option_list + hr_line + unknown_option_list + hr_line + plan_option_list;
+    $('#domainPlanimationSelection').html(domain_list);
+    $('#problemPlanimationSelection').html(problem_list);
+    $('#animateSelection').html(animate_list);
+    $('#planSelection').html(plan_list);
+    if (setDom)
+      $('#domainPlanimationSelection').val(window.last_domain);
+    if (setProb)
+      $('#problemPlanimationSelection').val(window.last_problem);
+    if (setAnimate)
+      $('#animateSelection').val(window.last_animate);
+    if (setPlan)
+      $('#planSelection').val(window.last_plan);
+    $('#chooseFilesPlanimationModel').modal('toggle');
+  }
+  
+  
+  
+  function on_change(event) {
+    if (event.id == "planradio") {
+      $('#planSelection').show();
+    } else {
+      $('#planSelection').hide();
+    }
+  
+  }
+
+// Called when you click 'Go' on the file chooser
+function loadStatespace() {
+
+    // Getting string versions of the selected files
+    var domain = window.ace.edit($('#domainPlanimationSelection').find(':selected').val()).getSession().getValue();
+    var problem = window.ace.edit($('#problemPlanimationSelection').find(':selected').val()).getSession().getValue();
+    var animation = window.ace.edit($('#animateSelection').find(':selected').val()).getSession().getValue();
+    window.heuristicVizDomain = domain;
+    window.heuristicVizProblem = problem;
+    window.heuristicVizAnimation = animation;
+
+    // Lowering the choose file modal menu
+    $('#chooseFilesPlanimationModel').modal('toggle');
+    $('#plannerURLInput').show();
+
+    // Ground the domain and problem
+    ground(domain, problem).then(function(result) {
+        treeData = {"name":"root", "children":[], "state":result.state, "strState":result.strState, "precondition":null, "loadedChildren":false};
+        stateCounter = 1;
+        launchViz();
+    });
+}
+
 define(function () {
     window.d3_loaded = false;
   return {
-      name: "Heuristic Viz",
+      name: "Heuristic Viz with Planimation",
       author: "Caitlin Aspinall, Cam Cunningham & Ellie Sekine",
       email: "16cea5@queensu.com",
       description: "Heuristic Visualization",
@@ -1065,7 +1220,7 @@ define(function () {
         initializeGrounding();
 
         // Adds menu button that allows for choosing files
-        window.add_menu_button('Viz', 'vizMenuItem', 'glyphicon-tower',"chooseFiles('viz')");
+        window.add_menu_button('Viz', 'vizMenuItem', 'glyphicon-tower',"choosePlanimationFiles('viz')");
         window.inject_styles('.viz_display {padding: 20px 0px 0px 40px;}')
 
         // Register this as a user of the file chooser interface
@@ -1075,12 +1230,25 @@ define(function () {
                 // Button name, Description
                 window.setup_file_chooser('Go', 'Display Visualization');
                 $('#plannerURLInput').hide();
+                window.action_type = 'viz'
+                 $('#plannerPlanimationURL').val(window.planimationURL);
+
             },
             // Called when go is hit
             selectChoice: loadStatespace
         });
+
+
+      if (!(window.planimationSolverStyled)) {
+        $('body').append(PLANIMATION_MODEL);
+
+        window.planimationSolverStyled = true;
+      }
+
+
         },
 
+        
         disable: function() {
           // This is called whenever the plugin is disabled
           window.toastr.warning("Plug in disabled")
@@ -1098,3 +1266,6 @@ define(function () {
         }
   };
 });
+
+
+
